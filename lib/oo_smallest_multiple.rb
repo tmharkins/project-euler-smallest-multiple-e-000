65f0248d8394a81ce692bb9a858020edcedaf441
tmharkins <tmharkins@gmail.com>
2016-01-11 05:57:31 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+	def initialize(input)
+		@x = 1
+    @input = input
+		@num = input - 1
+		@y = @num
+	end
+
+	def lcm
+		until @y == 2
+			multiple = @input * @x
+			if multiple % @y == 0
+				#divider found, go until down to 3
+				@y -= 1
+			else
+				#reset y, increment x and try again
+				@x += 1
+				@y = @num
+			end
+		end
+
+		p "smallest multiple = #{multiple}"
+		return multiple
+	end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,21 @@
-# Implement your procedural solution here!
+def smallest_multiple(input)
+	x = 1
+	num = input - 1
+	y = num
+
+	p "start until"
+	until y == 2
+		prod = input * x
+#		prod = 10 * x
+		if prod % y == 0
+			#divider found, go until down to 3
+			y -= 1
+		else
+			#reset y, increment x and try again
+			x += 1
+			y = num
+		end
+	end
+	p "smallest multiple = #{prod}"
+	return prod
+end
